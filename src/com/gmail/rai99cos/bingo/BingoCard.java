4a09287e95cbfd970a48ea3da2c71bf508a1179e
NONONOexe <g.kei0429@gmail.com>
2016-01-03 14:16:15 +0900

    Fix Code まだカンペキではないですがswingらしい書き方に少し変更してみました。 ビンゴのような表(Matrix)を扱うものはGridLayoutを使用すると良いと思います。 今回はBorderLayoutで次のように作りなおしました。  - NORTH(北) : JLabel "BINGO"(ビンゴのタイトル)  - CENTER(中央) : JPanel numbers(番号の表), GridLayout使用

--- src/com/gmail/rai99cos/bingo/BingoCard.java
+++ src/com/gmail/rai99cos/bingo/BingoCard.java
@@ -1,7 +1,9 @@
 package com.gmail.rai99cos.bingo;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -14,66 +16,55 @@ import javax.swing.JPanel;
 public class BingoCard {
 
 	public static void main(String[] args) {
-		// swing here
 		JFrame jframe = new JFrame("BINGO!");
 		JPanel jpanel = new BingoJPanel();
 
-		Logic logic = new Logic();
-		logic.CreatNum();
+		BingoMatrixGenerator logic = new BingoMatrixGenerator();
+		logic.creatBingoMatrix();
 
-		List<JButton> buttonList = new ArrayList<>();
-		for (int i = 0; i < 25; i++) {
-			buttonList.add(new JButton(logic.getNums(i)));
-		}
-		buttonList.remove(12);
-		buttonList.add(12, new JButton("FREE"));
+		List<JButton> buttons = new ArrayList<>(BingoMatrixGenerator.SIZE);
+		for (int i = 0; i < BingoMatrixGenerator.SIZE; i++) {
+			JButton numberButton = null;
 
-		Font font1 = new Font("Century", Font.BOLD, 45);
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 5; j++) {
-				if (i == 2 && j == 2) {
-					buttonList.get((i * 5) + j).setFont(new Font("Century", Font.BOLD, 25));
-				} else {
-					buttonList.get((i * 5) + j).setFont(font1);
-				}
-				buttonList.get((i * 5) + j).setBounds((i * 100) + 10, (j + 1) * 100, 95, 95);
-				buttonList.get((i * 5) + j).setForeground(Color.black);
-				buttonList.get((i * 5) + j).addActionListener(new ActionListener() {
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						/*
-						 * ここを変更しました
-						 * Background : 背景
-						 * Foreground : 文字色
-						 * defaultColorはjbutton.getBackground()をsysoutして
-						 * 確認しました
-						 * ColorクラスはコンストラクタでRGB設定が可能です
-						 *  -> Color sample = new Color(128, 128, 128);//gray
-						 */
-						JButton jbutton = (JButton) e.getSource();
-						Color defaultColor = new Color(238, 238, 238);
-						Color pushedColor = Color.RED;
-						if (jbutton.getBackground().equals(defaultColor)) {
-							jbutton.setBackground(pushedColor);
-						} else {
-							jbutton.setBackground(defaultColor);
-						}
+			if (i != BingoMatrixGenerator.CENTER_INDEX) {
+				int number = logic.getNumber(i);
+				numberButton = new JButton(String.valueOf(number));
+				Font buttonFont = new Font("Century", Font.BOLD, 45);
+				numberButton.setFont(buttonFont);
 
-						//if (jbutton.getForeground() == Color.black) {
-						//	jbutton.setForeground(Color.red);
-						//} else {
-						//	jbutton.setForeground(Color.black);
-						//}
-
-					}
-				});
-				jframe.add(buttonList.get((i * 5) + j));
+			} else {
+				numberButton = new JButton("FREE");
+				numberButton.setFont(new Font("Century", Font.BOLD, 20));
 			}
+			numberButton.setBackground(Color.WHITE);
+			numberButton.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					JButton jbutton = (JButton) e.getSource();
+					Color defaultColor = Color.WHITE;
+					Color pushedColor = Color.RED;
+					if (jbutton.getBackground().equals(defaultColor)) {
+						jbutton.setBackground(pushedColor);
+					} else {
+						jbutton.setBackground(defaultColor);
+					}
+				}
+			});
+
+			buttons.add(numberButton);
 		}
 
+		JPanel numbersPanel = new JPanel();
+		numbersPanel.setLayout(new GridLayout(BingoMatrixGenerator.HEIGHT, BingoMatrixGenerator.WIDTH));
+		for(JButton button : buttons){
+			numbersPanel.add(button);
+		}
+
+		jpanel.add(numbersPanel, BorderLayout.CENTER);
 		jframe.add(jpanel);
 		jpanel.setBackground(Color.green);
-		jframe.setSize(515, 627);
+		jframe.setSize(500, 700);
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jframe.setVisible(true);
 	}
--- src/com/gmail/rai99cos/bingo/BingoJPanel.java
+++ src/com/gmail/rai99cos/bingo/BingoJPanel.java
@@ -1,21 +1,21 @@
 package com.gmail.rai99cos.bingo;
 
+import java.awt.BorderLayout;
 import java.awt.Font;
-import java.awt.Graphics;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class BingoJPanel extends JPanel {
-	@Override
-	public void paint(Graphics g) {
-		super.paint(g);
-		Font font1 = new Font("Century", Font.BOLD, 90);
-		g.setFont(font1);
-		g.drawString("B", 30, 85);
-		g.drawString("I", 140, 85);
-		g.drawString("N", 220, 85);
-		g.drawString("G", 325, 85);
-		g.drawString("O", 425, 85);
+	
+	public BingoJPanel() {
+		this.setLayout(new BorderLayout());
+		
+		JLabel label = new JLabel("BINGO");
+		Font font = new Font("Century", Font.BOLD, 90);
+		label.setFont(font);
+		label.setHorizontalAlignment(JLabel.CENTER);
+		this.add(label, BorderLayout.NORTH);
 	}
 }
